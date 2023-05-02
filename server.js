efdd16ec428e4303fb4eb62b36e2dab874e17fcc
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-05-02 16:08:47 +0300

    Update

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
   const app = require("./app");
   const { DB_HOST, PORT = 5000 } = process.env;
   
@@ -20,19 +19,4 @@ const mongoose = require("mongoose");
 
 
 
-  const app = require("./app");
-  const { DB_HOST, PORT = 5000 } = process.env;
   
-  mongoose.set("strictQuery", true);
-  
-  mongoose
-    .connect(DB_HOST)
-    .then(() =>
-      app.listen(PORT, () => {
-        console.log("Database connection successful");
-      })
-    )
-    .catch((error) => {
-      console.log(error.message);
-      process.exit(1);
-    });
