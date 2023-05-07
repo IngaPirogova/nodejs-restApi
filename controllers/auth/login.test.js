911eedc3a9febfe254c20f64fdddaee96e3f8f9a
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-05-07 22:11:12 +0300

    upd

--- controllers/auth/login.test.js
+++ controllers/auth/login.test.js
@@ -1,15 +1,30 @@
-const express = require("express");
 const request = require("supertest");
-const app = express();
-const login = require("./login");
-app.post("/api/auth", login)
+const app = require('../../app');
+const mongoose = require('mongoose');
+
+const { PORT = 5000 } = process.env;
 
 describe("test login controller", ()=> {
-    beforeAll(() => app.listen(5000));
-    afterAll(() => app.close());
+    beforeAll(() => app.listen(PORT));
+    beforeEach(() => {
+        console.log("befoEach");
     });
-
-    test("response should have status code 200, return a token and a user object with email and subscription", async() => {
-        const response = await (await request(app).post("/api/auth/login"))
-        console.log(response)
+    afterAll( async () => {
+        await mongoose.connection.close();
+        await app.close();
+    });
+    
+   
+    test("response should have status code 200, return a token and a user object with email and subscription", 
+    async() => {
+        const userTest = {
+            "email": "usertest@gmail.com",
+            "password": "password"
+        }
+        const response = (await request(app).post("/api/auth/login")).send(userTest);
+        console.log(response.statusCode, response.body);
+        expect(response.status).toBe(200);
     })
+});
+
+
